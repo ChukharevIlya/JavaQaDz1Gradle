16709ed5d9fbd7584d40511f8d9646c931461556
ChukharevIlya <chukharevowen@yandex.ru>
2022-08-31 15:08:20 +0300

    junit5

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,10 @@
 package ru.netology.service;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertEquals;
+
+//import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CashbackHackServiceTest {
 
@@ -44,6 +47,54 @@ public class CashbackHackServiceTest {
     //середина диапазона
     @Test
     public void test4() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1500);
+        int expected = 500;
+
+        assertEquals(actual, expected);
+    }
+
+    // купил на 999 ответ 1
+    @Test
+    public void test5() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(999);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+    }
+
+    // купил на 1000 ответ 0
+    @Test
+    public void shouldNotAskToIncreaseTheAmountIfDivisibleByBoundary2() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    // купил на 1001 ответ 999
+    @Test
+    public void test6() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1001);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+    }
+
+    //середина диапазона
+    @Test
+    public void test7() {
 
         CashbackHackService service = new CashbackHackService();
 
